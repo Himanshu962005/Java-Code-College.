d3e37adbd4fe3f294b4c378c79d62bce98a3edc1
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-09-08 17:21:48 +0530

    Add files via upload

---
+++ Main12.java
@@ -0,0 +1,16 @@
+class Main12 {
+    public static void main(String[] args) {
+        StringBuffer string = new StringBuffer();
+        char c;
+        System.out.println("Enter a string : ");
+        try {
+            while ((c = (char) System.in.read()) != '\n') {
+                string.append(c); // Append Character.
+            }
+        } catch (Exception e) {
+            System.out.println("Error in input");
+        }
+        System.out.println("You have entered...");
+        System.out.println(string);
+    }
+}
