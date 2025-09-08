2c45dc3be68163ecadf683452819a7ff6ef698e9
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-09-08 18:58:33 +0530

    Add files via upload

---
+++ Main15.java
@@ -0,0 +1,12 @@
+class Main15 {
+    public static void main(String[] args) {
+        int p, q;
+        System.out.println("The right angle triangle of @ is shown below : ");
+        for (p = 1; p <= 9; p++) {
+            for (q = 1; q <= p; q++) {
+                System.out.print("@");
+            }
+            System.out.println(" ");
+        }
+    }
+}
