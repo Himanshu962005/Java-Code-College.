06280bbec26fc1ec500a99db146033452ce452d0
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-09-09 17:08:53 +0530

    Add files via upload

---
+++ Main24.java
@@ -0,0 +1,32 @@
+class Super {
+    int x;
+
+    Super(int x) {
+        this.x = x;
+    }
+
+    void display() {
+        System.out.println("Super x = " + x);
+    }
+}
+
+class Sub extends Super {
+    int y;
+
+    Sub(int x, int y) {
+        super(x);
+        this.y = y;
+    }
+
+    void display() {
+        System.out.println("Super x = " + x);
+        System.out.println("Sub y = " + y);
+    }
+}
+
+class Main24 {
+    public static void main(String[] args) {
+        Sub sl = new Sub(100, 200);
+        sl.display();
+    }
+}
