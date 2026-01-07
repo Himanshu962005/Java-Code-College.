7061392ecafc3d658c4eca013b942592678ae372
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-07 20:43:01 +0530

    Update Main56.java

--- Main56.java
+++ Main56.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main56 {
 	public static void main(String[] args) {
-		int[] arr = { 10, 5, 8, 20, 3, 15, };
+		int[] arr = { 10, 5, 8, 20, 3, 15 };
 		Arrays.sort(arr);
 		System.out.println("Sorted Array : " + Arrays.toString(arr));
 		int SecondSmallest = arr[1];
