6989bfdb97f9b8fdcf11c9b278d5babde54f0a70
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-07 20:39:05 +0530

    Add files via upload

---
+++ Main56.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main56 {
+	public static void main(String[] args) {
+		int[] arr = { 10, 5, 8, 20, 3, 15, };
+		Arrays.sort(arr);
+		System.out.println("Sorted Array : " + Arrays.toString(arr));
+		int SecondSmallest = arr[1];
+		int SecondLargest = arr[arr.length - 2];
+		System.out.println("Second Smallest : " + SecondSmallest);
+		System.out.println("Second Largest : " + SecondLargest);
+	}
+}
