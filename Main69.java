a0fc16fd34280610a296463f7a29093ca382a09f
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-17 18:18:28 +0530

    Add files via upload

---
+++ Main69.java
@@ -0,0 +1,8 @@
+class Main69 { // CommandLineDemo.
+    public static void main(String args[]) {
+        System.out.println("Number of Arguments : " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + " : " + args[i]);
+        }
+    }
+}
