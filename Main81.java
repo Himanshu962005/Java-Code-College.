810396a5c3bb6ea67216715ccc27258d8acc17df
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-19 21:44:46 +0530

    Add files via upload

---
+++ Main81.java
@@ -0,0 +1,8 @@
+// Write a Java program to replace spaces with underscores.
+class Main81 { // ReplaceSpaces.
+    public static void main(String[] args) {
+        String str = "Java is very Easy";
+        String result = str.replace(" ", "_");
+        System.out.println(result);
+    }
+}
