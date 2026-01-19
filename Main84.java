5a675ec25833be87358d33b8ed5f46fdaaeb620c
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-19 22:10:00 +0530

    Add files via upload

---
+++ Main84.java
@@ -0,0 +1,8 @@
+/* Write a program to format the following letter using escape sequence characters.
+Letter = "Dear Himanshu, This Java Course is nice. Thanks" */
+class Main84 { // FormatLetter.
+    public static void main(String[] args) {
+        String letter = "Dear Himanshu,\n\tThis Java Course is Nice.\nThanks";
+        System.out.println(letter);
+    }
+}
