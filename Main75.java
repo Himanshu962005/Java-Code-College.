855634ca3d53748fa621837e23f60abae1d5d96e
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-19 17:17:50 +0530

    Add files via upload

---
+++ Main75.java
@@ -0,0 +1,7 @@
+// What will be the result of the following expression float a = 7/4*9/2.
+class Main75 { // ExpressionResult.
+    public static void main(String[] args) {
+        float a = 7 / 4 * 9 / 2;
+        System.out.println("Value of A = " + a);
+    }
+}
