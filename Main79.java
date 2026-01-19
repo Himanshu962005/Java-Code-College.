2801e2e849bfd22c5e081e54f4a2058094b3c600
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-19 18:59:21 +0530

    Add files via upload

---
+++ Main79.java
@@ -0,0 +1,7 @@
+// Find the value of the following expression : int x = 7; int a = 7 * 49 / 7 + 35 / 7; Value of A.
+class Main79 { // ValueOfA.
+    public static void main(String[] args) {
+        int a = 7 * 49 / 7 + 35 / 7;
+        System.out.println("Value of A = " + a);
+    }
+}
