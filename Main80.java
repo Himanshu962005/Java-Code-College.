33d582fe7138164eb0783a7b737ce5ea5b903e4e
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-19 21:37:04 +0530

    Add files via upload

---
+++ Main80.java
@@ -0,0 +1,8 @@
+// Write a Java program to convert a string to lowercase.
+class Main80 { // StringToLowercase.
+    public static void main(String[] args) {
+        String str = "HELLO JAVA WORLD";
+        String lower = str.toLowerCase();
+        System.out.println("Lowercase String : " + lower);
+    }
+}
