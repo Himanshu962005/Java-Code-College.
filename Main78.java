fcd39568fa2754f8187f30b1fef93225c4e588b9
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-19 18:40:50 +0530

    Add files via upload

---
+++ Main78.java
@@ -0,0 +1,11 @@
+// Write the following expression in a Java program : (v^2 - u^2) / (2as).
+class Main78 { // ExpressionFormula.
+    public static void main(String[] args) {
+        int v = 10;
+        int u = 5;
+        int a = 2;
+        int s = 4;
+        int result = (v * v - u * u) / (2 * a * s);
+        System.out.println("Result = " + result);
+    }
+}
