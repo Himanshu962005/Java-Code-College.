6a2e245ee4b0dcf33073e79d874b5bff13e6c6ad
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-20 18:40:02 +0530

    Add files via upload

---
+++ Main93.java
@@ -0,0 +1,8 @@
+// Write a program to print multiplication table of 10 in reverse order.
+class Main93 { // TableReverse.
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("10 x " + i + " = " + (10 * i));
+        }
+    }
+}
