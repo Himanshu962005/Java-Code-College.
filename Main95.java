af82d6235788d814c278aca85c8b1109fcfe868d
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-01-20 19:02:47 +0530

    Add files via upload

---
+++ Main95.java
@@ -0,0 +1,10 @@
+// Write a program to calculate the sum of the numbers occurring in the multiplication table of 8.
+class Main95 { // SumTable8.
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += 8 * i;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
