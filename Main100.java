3150044b318ab2856fcdee3cb312fdcaaeea2bbd
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-21 14:46:48 +0530

    Update Main100.java
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

--- Main100.java
+++ Main100.java
@@ -1,5 +1,5 @@
 // Write a Java program to reverse an array.
-class Main100 { // ReverseArray.
+class Main100 { // Reverse Array.
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
         for (int i = arr.length - 1; i >= 0; i--) {
